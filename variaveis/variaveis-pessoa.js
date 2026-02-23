b3ee0f4e2066e08a155e053d8625d508a1a3e687
vitor467983 <vitorladwig9@gmail.com>
2026-02-23 12:35:41 +0000

    criar variaveis  para pessoas e produtos

---
+++ variaveis/variaveis-pessoa.js
@@ -0,0 +1,27 @@
+// , var, let, const (valores reservados da linguagem)
+// 0-9, 0,7, 0-1, 0-9,A,B,C,D,E,F
+
+let nome = "Peter"           // string
+let sobrenome = "Parker"     // string
+const cpf = "888888888-88"   // string
+const rg = "101212 spp"      // string
+let idade = 22               // string
+let solteiro = true          // boolean
+const nota = 7 + 2           // number
+
+console.log(nome)
+ // imprima aqui todas as variaveis acima
+console.log(sobrenome)
+console.log(cpf)
+console.log(rg)
+console.log(idade)
+console.log(solteiro)
+console.log(nota)
+
+console.log(typeof nome)
+console.log(typeof sobrenome)
+console.log(typeof cpf)
+console.log(typeof rg)
+console.log(typeof idade)
+console.log(typeof solteiro)
+console.log(typeof nota)
---
+++ variaveis/variavel-produto.js
@@ -0,0 +1,17 @@
+let nome = "Creatina"
+let sobrenome = "Monohidratada"
+let quantidade = '500g'
+let preço = "79,90"
+let disponivel = true
+
+console.log(nome)
+console.log(sobrenome)
+console.log(quantidade)
+console.log(preço)
+console.log(disponivel)
+
+console.log(typeof nome)
+console.log(typeof sobrenome)
+console.log(typeof quantidade)
+console.log(typeof preço)
+console.log(typeof disponivel)
