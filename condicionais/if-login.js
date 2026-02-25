e39835bc08776d56dc1c55ae9407208047022549
vitor467983 <vitorladwig9@gmail.com>
2026-02-25 11:35:38 +0000

    Criar expressões de decisão condicional if-else

---
+++ condicionais/if-login.js
@@ -0,0 +1,13 @@
+let login = "gt123"
+let senha = 123456
+
+if (login == "gt123") {
+    console.log("Login, correto!")
+}else if(senha >= 6 && senha <= 100){
+console.log("Bem, vindo(a)")
+console.log("")
+}
+ else {
+    console.log("Login, incorreto!")
+    console.log("Senha, incorreta!")
+}
---
+++ condicionais/if_else.js
@@ -0,0 +1,17 @@
+// if, else, else if, ternário
+// serve para tomada de decisão (se/senão)
+// OPERADORES DE COMPARAÇÂO >, <, >=, <=, !=
+// OPERADORES LÓGICOS: || (OU lógico), && (E lógico)
+
+
+let idadePessoa = 19
+
+if (idadePessoa > 100) {
+console.log("Favor, apresentar companhante!")
+}else if(idadePessoa >= 17 && idadePessoa <= 100){
+    console.log("Bem vindo(a), a festa é sua!")
+    console.log("Festa sugerida > Boate!!!")
+}
+else{
+console.log("Não é festa para menor de idade!")
+}
