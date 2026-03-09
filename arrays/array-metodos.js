e61ec5c0a0e31609e24be8b1157f409c866a6148
vitor467983 <vitorladwig9@gmail.com>
2026-03-09 11:01:49 +0000

    .

---
+++ arrays/array-metodos.js
@@ -0,0 +1,17 @@
+// crie um array que contenha 3 frutas
+// adicione 4 frutas com o metodo .push(<nomeFruta>)
+// remova á ultima fruta com o método .pop()
+// faça um loop que remova todas as frutas
+// verifique, se todas as frutas foram removidas
+// imprima "nenhuma fruta encontrada"
+
+const frutas = ["Pera", "banana", "maça"]
+console.log(frutas)
+frutas.push("caqui")
+frutas.push("graviola")
+frutas.push("carambola")
+frutas.push("lichia")
+
+console.log(frutas)
+frutas.pop()
+console.log(frutas)
--- arrays/array-time.js
+++ arrays/array-time.js
@@ -4,7 +4,8 @@ time[5] // undefined
 time[6] = 'Victor' // atribuição dinâmicos
 
 for (const jogador of time) {
+    if (jogador != undefined)
     console.log(jogador)
-}
 console.log(time)
 console.log(time[1])
+}
