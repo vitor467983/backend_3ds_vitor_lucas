ab05e47f73945275fd99ba7001cbbac4c1abe45b
vitor467983 <vitorladwig9@gmail.com>
2026-03-30 11:01:42 +0000

    .

---
+++ funcoes/func-01.js
@@ -0,0 +1,16 @@
+// Funções são blocos de códigos reaproveitáveis
+// podem ou não receber parâmetros
+// podem ou não retornar valores
+
+// declaração 
+function dizOla(){
+    console.log("Olá")
+}
+function olaPessoa(nome){
+    console.log("Olá, " + nome)
+}
+
+//invocação
+dizOla()
+olaPessoa("Luana")
+olaPessoa("Mano")
---
+++ funcoes/func-02.js
@@ -0,0 +1,26 @@
+// Crie 4 funções para operações (+, -, *, /)
+// Calcule e imprima resultado
+
+function subtracao(n1, n2){
+    const resultado = n1 - n2
+    console.log("Resultado da subtração: " + resultado)
+}
+// inovação
+subtracao(7, 3)
+subtracao(-30, 20)
+
+function multiplicacao(n1, n2){
+    const resultado = n1 * n2
+    console.log("Resultado da multiplicação: " + resultado)
+}
+// inovação
+multiplicacao(10, 3)
+multiplicacao(10, 20)
+
+function divisao(n1, n2){
+    const resultado = n1 + n2
+    console.log("Resultado da divisão: " + resultado)
+}
+// inovação
+divisão(10, 3)
+divisão(-50, 20)
