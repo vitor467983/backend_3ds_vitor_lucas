6bd5d47b552ecd857429a007f72b1d4e83887266
vitor467983 <vitorladwig9@gmail.com>
2026-04-08 12:29:17 +0000

    .

--- funcoes/fun-04.js
+++ funcoes/fun-04.js
@@ -9,5 +9,9 @@ function sobraDoSalario(meuSalario){
     const contaDeluz = 150.0
     const aluguel = 1400.0
     const resto = meuSalario - (contaDeluz+aluguel)
-    console.log(`O que restou no fim do mês ${resto}`)
+    return `O que restou no fim do mês ${resto}`
 }
+const minhasSobras = sobraDoSalario(1700.0)
+// teste - experimento
+console.log(sobraDoSalario(19++000.0))
+console.log(minhasSobras)
