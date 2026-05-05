7d30f5683e1c718bf6438aff13f66911b00bf73e
vitor467983 <vitorladwig9@gmail.com>
2026-05-05 13:47:41 +0000

    .

---
+++ 2°TRIMESTRE/modulo/main-calc.js
@@ -0,0 +1,7 @@
+// CommonJS é o sistema nativo de
+//  módulos do nodejs (.cjs)
+// Exige Exports/Require
+
+const calc = require('./mod-calc.js')
+calc(5,5)
+console.log(calc(5,5))
---
+++ 2°TRIMESTRE/modulo/mod-calc.js
@@ -0,0 +1,28 @@
+// module.exports = function muiltiplicaPorDois (n1,n2){
+//     const res = `resultado ${n1 * n2}`
+//     return res
+// }
+
+function dividePorDois (n1,n2){
+    const res = `resultado ${n1 / n2}`
+    return res
+}
+function multiplicaPorDois (n1,n2){
+    const res = `resultado ${n1 * n2}`
+    return res
+}
+function somaPorDois (n1,n2){
+    const res = `resultado ${n1 + n2}`
+    return res
+}
+function subtraiPorDois (n1,n2){
+    const res = `resultado ${n1 - n2}`
+    return res
+}
+
+module.exports = {
+    dividePorDois
+    multiplicaPorDois
+    somaPorDois
+    subtraiPorDois
+}
