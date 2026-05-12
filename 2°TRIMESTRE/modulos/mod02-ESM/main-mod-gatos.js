a1398a5ca9bea98c8793aa0b33f1db8cb1d3cfed
vitor467983 <vitorladwig9@gmail.com>
2026-05-12 13:40:22 +0000

    organizar pastas do primeiro e segundo trimestres e pastas de modulos

---
+++ 2°TRIMESTRE/modulos/mod02-ESM/main-mod-gatos.js
@@ -0,0 +1,14 @@
+let racaGato = `Vira-lata`
+
+export function banhoGATO(racaGato, tipoBanho){
+
+}
+
+export function tosaGATO(racaGato, tipoTosa){
+
+    
+}
+
+export function alimentarGATO(racaGato, tipoRacao){
+    
+}
