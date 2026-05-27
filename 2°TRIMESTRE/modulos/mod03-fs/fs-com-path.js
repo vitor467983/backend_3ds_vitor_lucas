eda4c9fe2464c60e04983cc17454490cbcad7159
vitor467983 <vitorladwig9@gmail.com>
2026-05-27 11:48:20 +0000

    finalização do fs com path (join e resolve)

--- 2°TRIMESTRE/modulos/mod03-fs/fs-com-path.js
+++ 2°TRIMESTRE/modulos/mod03-fs/fs-com-path.js
@@ -1,9 +1,11 @@
 // importar path e fs
-
-
+// Path possui (join para juntar caminhos e Resolve
+// para deduzir subdiretorios e S.O)
+const fs = require('fs')
+const path = require('path')
 
 // salvar o Caminho em um variavel
-
-
-
-// Abrir, ler, deletar, editar
+// const caminho = path.join(__dirname, 'README.md')//Markdown, text,
+const caminho = path.resolve(__dirname,'README.md' )// Abrir, ler, deletar, editar
+const arquivo = fs.readFileSync(caminho, 'utf-8')
+console.log(arquivo)
