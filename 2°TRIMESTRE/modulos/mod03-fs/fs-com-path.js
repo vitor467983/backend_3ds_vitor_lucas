628a890471f82552cbec1865cb7f4ea5b47de1ff
vitor467983 <vitorladwig9@gmail.com>
2026-05-27 11:51:18 +0000

    finalização

--- 2°TRIMESTRE/modulos/mod03-fs/fs-com-path.js
+++ 2°TRIMESTRE/modulos/mod03-fs/fs-com-path.js
@@ -6,6 +6,6 @@ const path = require('path')
 
 // salvar o Caminho em um variavel
 // const caminho = path.join(__dirname, 'README.md')//Markdown, text,
-const caminho = path.resolve(__dirname,'README.md' )// Abrir, ler, deletar, editar
+const caminho = path.resolve(__dirname,'README.md' )/// Abrir, ler, deletar, editar
 const arquivo = fs.readFileSync(caminho, 'utf-8')
 console.log(arquivo)
