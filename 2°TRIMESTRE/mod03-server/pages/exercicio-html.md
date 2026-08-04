74275bb91e01f32c6bcf9e042dd071a0b26c729c
vitor467983 <vitorladwig9@gmail.com>
2026-08-04 11:51:11 +0000

    Adicionar páginas HTML e configurar rotas no servidor

---
+++ 2°TRIMESTRE/mod03-server/pages/exercicio-html.md
@@ -0,0 +1,7 @@
+### Exercício HTML e ROTAS
+
+- A partir da estrutura desta pasta "SERVER"
+ - Crie HTML com TEMA LIVRE PARA AS PAGINAS:
+ - index.html, sobre.html, 404.html (página não encontrada)
+ - * GARANTA QUE O LINK DE NAVEGAÇÃO FUNCIONE 
+ - * CSS PODE SER INLINE -> dentro do Head!
--- 2°TRIMESTRE/mod03-server/pages/server01.js
+++ 2°TRIMESTRE/mod03-server/pages/server01.js
@@ -1,14 +1,21 @@
-const http = require('node:http')
-const porta = 8081
+const home = path.join(__dirname, 'pages/index.html')
+const sobre = path.join(__dirname, 'pages/sobre.html')
 
 const server = http.createServer((req, res)=>{
     const novaUrl = new URL(req.url, `http://${req.headers.host}`) //http://localhost
     const caminhoUrl = novaUrl.pathname
+
     if(caminhoUrl === '/'){
  // o que vem da url?
- res.statusCode = '201'
+ res.statusCode = '200'
  res.setHeader('Content-Type', 'text/html; charset=utf-8')
- res.end('<h3>HTML Rocks!</h3>')
+ return res.end(fs.readFileSync(home, 'utf-8')) // chamar o HTML
+ }
+ if(caminhoUrl === '/sobre'){
+ // o que vem da url?
+ res.statusCode = '200'
+ res.setHeader('Content-Type', 'text/html; charset=utf-8')
+ return res.end(fs.readFileSync(sobre, 'utf-8'))
     } else{
 res.statusCode = '401'
 res.setHeader('Content-Type', 'text/html', 'charset=utf-8')
