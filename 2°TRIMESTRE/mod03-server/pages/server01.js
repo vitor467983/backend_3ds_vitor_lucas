c1ccf2212ebf0b4d37c5a51ad793bf9612c5e60a
vitor467983 <vitorladwig9@gmail.com>
2026-08-11 12:18:18 +0000

    Adicionar páginas HTML de 404, index e sobre, e atualizar servidor para gerenciar novas rotas

--- 2°TRIMESTRE/mod03-server/pages/server01.js
+++ 2°TRIMESTRE/mod03-server/pages/server01.js
@@ -1,30 +1,33 @@
-const home = path.join(__dirname, 'pages/index.html')
-const sobre = path.join(__dirname, 'pages/sobre.html')
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+const porta = process.env.PORT || 3000
 
-const server = http.createServer((req, res)=>{
-    const novaUrl = new URL(req.url, `http://${req.headers.host}`) //http://localhost
+const home = path.join(__dirname, 'index.html')
+const sobre = path.join(__dirname, 'sobre.html')
+const notFound = path.join(__dirname, '404.html')
+
+const server = http.createServer((req, res) => {
+    const novaUrl = new URL(req.url, `http://${req.headers.host}`)
     const caminhoUrl = novaUrl.pathname
 
-    if(caminhoUrl === '/'){
- // o que vem da url?
- res.statusCode = '200'
- res.setHeader('Content-Type', 'text/html; charset=utf-8')
- return res.end(fs.readFileSync(home, 'utf-8')) // chamar o HTML
- }
- if(caminhoUrl === '/sobre'){
- // o que vem da url?
- res.statusCode = '200'
- res.setHeader('Content-Type', 'text/html; charset=utf-8')
- return res.end(fs.readFileSync(sobre, 'utf-8'))
-    } else{
-res.statusCode = '401'
-res.setHeader('Content-Type', 'text/html', 'charset=utf-8')
-res.end('<h3>401 Não autorizado</h3>')
+    if (caminhoUrl === '/' || caminhoUrl === '/index.html') {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/html; charset=utf-8')
+        return res.end(fs.readFileSync(home, 'utf-8'))
     }
 
+    if (caminhoUrl === '/sobre' || caminhoUrl === '/sobre.html') {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/html; charset=utf-8')
+        return res.end(fs.readFileSync(sobre, 'utf-8'))
+    }
+
+    res.statusCode = 404
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+    return res.end(fs.readFileSync(notFound, 'utf-8'))
 })
 
-server.listen(porta, ()=>{
-    console.log(`Servidor rodando na porta 
-        http://localhost:${porta}`)
+server.listen(porta, () => {
+    console.log(`Servidor rodando em http://localhost:${porta}`)
 })
