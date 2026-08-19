1f9b46fcd3ff95677f46c914a8831b02905a16cb
vitor467983 <vitorladwig9@gmail.com>
2026-08-19 11:52:10 +0000

    Adicionar servidor HTTP, páginas HTML e estilos, e incluir imagem do pinguim

--- 2°TRIMESTRE/mod03-server/server01.js
+++ 2°TRIMESTRE/mod03-server/server01.js
@@ -4,9 +4,10 @@ const path = require('path')
 
 const porta = 3000
 
-const home = path.join(__dirname, 'index.html')
-const sobre = path.join(__dirname, 'sobre.html')
-const notFound = path.join(__dirname, '404.html')
+const home = path.join(__dirname, 'pages/index.html')
+const sobre = path.join(__dirname, 'pages/sobre.html')
+const notFound = path.join(__dirname, 'pages/404.html')
+const css = path.join(__dirname, 'public/estilo.css')
 
 const server = http.createServer((req, res) => {
     const novaUrl = new URL(req.url, `http://${req.headers.host}`)
@@ -23,7 +24,16 @@ const server = http.createServer((req, res) => {
         res.setHeader('Content-Type', 'text/html; charset=utf-8')
         return res.end(fs.readFileSync(sobre, 'utf-8'))
     }
-
+    if (caminhoUrl === '/' || caminhoUrl === '/public/estilo.css') {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/css')
+        return res.end(fs.readFileSync(css, 'utf-8'))
+    }
+    if (caminhoUrl === '/' || caminhoUrl === '/images/pinguim.png') {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'image/png')
+        return res.end(fs.readFileSync(pinguim))
+    }
     res.statusCode = 404
     res.setHeader('Content-Type', 'text/html; charset=utf-8')
     return res.end(fs.readFileSync(notFound, 'utf-8'))
