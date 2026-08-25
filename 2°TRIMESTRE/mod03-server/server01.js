1b4c5a94c69b948f6616437e6f34c94eadef3161
vitor467983 <vitorladwig9@gmail.com>
2026-08-25 11:09:29 +0000

    Adicionei imagem do pinguim e ajustei rotas no servidor

--- 2°TRIMESTRE/mod03-server/server01.js
+++ 2°TRIMESTRE/mod03-server/server01.js
@@ -7,6 +7,7 @@ const porta = 3000
 const home = path.join(__dirname, 'pages/index.html')
 const sobre = path.join(__dirname, 'pages/sobre.html')
 const notFound = path.join(__dirname, 'pages/404.html')
+const pinguim = path.join(__dirname, 'images/pinguim.png')
 const css = path.join(__dirname, 'public/estilo.css')
 
 const server = http.createServer((req, res) => {
@@ -29,7 +30,7 @@ const server = http.createServer((req, res) => {
         res.setHeader('Content-Type', 'text/css')
         return res.end(fs.readFileSync(css, 'utf-8'))
     }
-    if (caminhoUrl === '/' || caminhoUrl === '/images/pinguim.png') {
+    if (caminhoUrl === '/images/pinguim.png') {
         res.statusCode = 200
         res.setHeader('Content-Type', 'image/png')
         return res.end(fs.readFileSync(pinguim))
